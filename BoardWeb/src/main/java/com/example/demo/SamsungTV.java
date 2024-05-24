f878cfeb57e25eebcb78fcffb6124de1744c42ab
dydwns <kaka2438@hanmail.net>
2024-05-24 19:33:01 +0900

    TV전원과 볼륨 UP, DOWN 코드

---
+++ BoardWeb/src/main/java/com/example/demo/SamsungTV.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public class SamsungTV {
+public void powerOn() {
+	System.out.println("SamsungTV---전원 켠다.");
+}
+public void powerOff() {
+	System.out.println("SamsungTV---전원 끈다.");
+	
+}
+public void volumUP() {
+	System.out.println("SamsungTV---소리 울린다.");
+}
+public  void volumDOWN() {
+	System.out.println("SamsungTV---소리 내린다.");
+}
+}
