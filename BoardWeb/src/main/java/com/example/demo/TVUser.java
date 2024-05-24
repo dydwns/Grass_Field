7ce61e6f01b7f22f3bccac2d528ab9bc47f35ef8
dydwns <kaka2438@hanmail.net>
2024-05-24 19:41:54 +0900

    두 클래스를 번갈아 사용하는 프로그램

---
+++ BoardWeb/src/main/java/com/example/demo/TVUser.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class TVUser {
+public static void main(String[] args){
+	SamsungTV tv = new SamsungTV();
+	tv.powerOn();
+	tv.volumUP();
+	tv.volumDOWN();
+	tv.powerOff();
+	
+}
+}
