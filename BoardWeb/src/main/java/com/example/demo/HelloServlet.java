291d446f5a0950b7e4919bf78d8edf7f66104695
dydwns <kaka2438@hanmail.net>
2024-05-24 18:47:04 +0900

    다시 올리기 위한

--- BoardWeb/src/main/java/com/example/demo/HelloServlet.java
+++ BoardWeb/src/main/java/com/example/demo/HelloServlet.java
@@ -2,12 +2,12 @@ package com.example.demo;
 
 import java.io.IOException;
 
-public class HelloServlet extends HttpServlet {
+public class HelloServlet extends HttpServlet. {
 public HelloServlet() {
 	System.out.println("===> HelloServlet 객체 생성");
 }
 
-protected void doGet (HttpServletRequest request,
+protected void doGet (HttpServletRequest. request,
 		HttpServletRespones response) throws ServletException, IOException {
 	System.out.println("doGet() 메소드 호출");
 }
